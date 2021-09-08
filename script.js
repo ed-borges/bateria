3bed9fce1e76992a7e991aeb468919024c8c21d9
Edvander Borges <edvander@live.com>
2021-09-08 12:05:04 +0100

    terminando o projecto

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-document.body.addEventListener('keyup', (event) => {
-    playSound(event.code.toLowerCase());
-})
+document.body.addEventListener("keyup", (event) => {
+  playSound(event.code.toLowerCase());
+});
+
+document.querySelector(".composer button").addEventListener("click", () => {
+  let song = document.querySelector("#input").value;
+
+  if (song !== "") {
+    let songArray = song.split("");
+    playComposition(songArray);
+  }
+});
+
+document.body.addEventListener("click", (event) => {
+    console.log(event.target);
+  });
 
 function playSound(sound) {
-    let audioElemnt = document.querySelector(`#s_${sound}`);
-    let keyElemnt = document.querySelector(`div[data-key="${sound}"]`);
+  let audioElemnt = document.querySelector(`#s_${sound}`);
+  let keyElemnt = document.querySelector(`div[data-key="${sound}"]`);
 
-    if (audioElemnt) {
-        audioElemnt.currentTime = 0;
-        audioElemnt.play();
-    }
-    if (keyElemnt) {
-        keyElemnt.classList.add('active');
+  if (audioElemnt) {
+    audioElemnt.currentTime = 0;
+    audioElemnt.play();
+  }
+  if (keyElemnt) {
+    keyElemnt.classList.add("active");
 
-        setTimeout(()=>{
-            keyElemnt.classList.remove('active');
-        }, 300)
-    }
+    setTimeout(() => {
+      keyElemnt.classList.remove("active");
+    }, 300);
+  }
+}
+
+function playComposition(songArray) {
+  let wait = 0;
+
+  for (let songItem of songArray) {
+    setTimeout(() => {
+      playSound(`key${songItem}`);
+    }, wait);
+
+    wait += 250;
+  }
 }
