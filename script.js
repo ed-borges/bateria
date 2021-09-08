bdc3bc33ab3dac6df6e774144ef151df2c4f4a29
Edvander Borges <edvander@live.com>
2021-09-08 12:20:10 +0100

    last update

---
+++ script.js
@@ -0,0 +1,41 @@
+document.body.addEventListener("keyup", (event) => {
+  playSound(event.code.toLowerCase());
+});
+
+document.querySelector(".composer button").addEventListener("click", () => {
+  let song = document.querySelector("#input").value;
+
+  if (song !== "") {
+    let songArray = song.split("");
+    playComposition(songArray);
+  }
+});
+
+function playSound(sound) {
+  let audioElemnt = document.querySelector(`#s_${sound}`);
+  let keyElemnt = document.querySelector(`div[data-key="${sound}"]`);
+
+  if (audioElemnt) {
+    audioElemnt.currentTime = 0;
+    audioElemnt.play();
+  }
+  if (keyElemnt) {
+    keyElemnt.classList.add("active");
+
+    setTimeout(() => {
+      keyElemnt.classList.remove("active");
+    }, 300);
+  }
+}
+
+function playComposition(songArray) {
+  let wait = 0;
+
+  for (let songItem of songArray) {
+    setTimeout(() => {
+      playSound(`key${songItem}`);
+    }, wait);
+
+    wait += 250;
+  }
+}
